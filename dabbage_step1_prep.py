0c1ca03d72348fb399ddd5b5c59e925028dcb58f
nathanhilbert <nathanhilbert@gmail.com>
2016-10-05 13:21:25 -0400

    fix dabbage preprocess and add uhi calculations

--- dabbage_step1_prep.py
+++ dabbage_step1_prep.py
@@ -6,6 +6,7 @@ import requests
 import rasterio
 from rasterio.mask import mask
 import copy
+import os
 
 from shapely.geometry import MultiPolygon, shape
 from rasterio.features import shapes
@@ -34,7 +35,7 @@ def processitem(placeid, sorig):
     earthenvtable = 'urbanclusters.earthenv_urbannamed'
 
 
-
+    print "doing the deepcopy"
     s = copy.deepcopy(sorig)
 
 
@@ -50,6 +51,7 @@ def processitem(placeid, sorig):
         FROM {0}
         WHERE placeid={1}
         """.format(earthenvtable, placeid)
+    print "running earthenv"
     r = engine.execute(GETGEOM)
     firstitem = r.first()
     if firstitem:
@@ -76,6 +78,7 @@ def processitem(placeid, sorig):
         FROM urbanclusters.usgscities as usgscities, urbanclusters.censusdabbagemsa as censusurb, urbanclusters.censusurbangroup as fullgeom
         WHERE ST_Contains(censusurb.geom, usgscities.geom) AND usgscities."gnis_id" = {0}
         """.format(s['gnisid'])
+    print "running the census msa geo tools with", s['gnisid']
     r = engine.execute(GETGEOM)
     firstitem = r.first()
     if firstitem:
@@ -92,11 +95,12 @@ def processitem(placeid, sorig):
             print s['usgsplacename']
             print s['gnisid']
 
-
+    print "loading the dem.tif file"
     demfile = "/Volumes/UrbisBackup/rasterstorage/dem/dem.tif"
     demsrc = rasterio.open(demfile)
     profile = demsrc.profile
 
+    print "loading the dem.json file"
     dempyspatialpath = "/Volumes/UrbisBackup/rasterstorage/dem/dem.json"
     dempyspatial = read_catalog(dempyspatialpath)
 
@@ -125,6 +129,7 @@ def processitem(placeid, sorig):
         urbmeanelevation = float((newv * neww).sum() / neww.sum())
 
         # now calculate the buffer section
+        print "doing the mask operation"
         result = mask(demsrc, [buffergeom], nodata=None, crop=True, all_touched=False, invert=False)
         z = np.where((result[0] > urbmeanelevation - 50) & (result[0] < urbmeanelevation + 50), 1, 0)
         p = z[0].astype('uint8')
@@ -174,7 +179,7 @@ def main():
     (array_agg(usgscities."gnis_id"  ORDER BY usgscities."pop_2010" DESC))[1] AS gnisid
     FROM urbanclusters.usgscities as usgscities, 
     urbanclusters.earthenv_urbannamed as earthenv
-    WHERE ST_Intersects(usgscities.geom, earthenv.geom) 
+    WHERE ST_Intersects(usgscities.geom, earthenv.geom) AND usgscities."gnis_id" > 0
     GROUP BY earthenv.placeid
     ORDER BY usgspopulation DESC
     LIMIT 100 """
@@ -196,13 +201,32 @@ def main():
     jobs = []
     
     for placeid, s in sampleplaces.iteritems():
+        if os.path.exists('Dabbage/pickles/{0}.pickle'.format(placeid)):
+            continue
         s[u'usgsplacegeomstr'] = str(s[u'usgsplacegeomstr'])
         s[u'usgsplacegeomwgs84str'] = str(s[u'usgsplacegeomwgs84str'])
+
+        # try:
+        #     print "starting on ", s['usgsplacename']
+        #     newplaceid, news = processitem(placeid, s)
+        # except Exception,e:
+        #     print e
+        # else:
+        #     with open('Dabbage/pickles/{0}.pickle'.format(newplaceid), 'wb') as fout:
+        #         pickle.dump(news, fout)
+
+
         jobs.append(pool.apply_async(processitem, [placeid, s]))
 
+    try:
+        os.mkdir("Dabbage/pickles")
+    except:
+        pass
     for job in jobs:
         newplaceid, news = job.get()
-        results[newplaceid] = news
+        with open('Dabbage/pickles/{0}.pickle'.format(newplaceid), 'wb') as fout:
+            pickle.dump(news, fout)
+
 
     # s = None
     # placeid = None
@@ -227,8 +251,7 @@ def main():
             
 
 
-    with open('Dabbage/preprocess_data.pickle', 'wb') as fout:
-        pickle.dump(results, fout)
+
 
 
 if __name__ == '__main__':
